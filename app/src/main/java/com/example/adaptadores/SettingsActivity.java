40009fb5a22d978e47301cd72306ce7ef3f11f21
JossAnn <jnatarenordonez@gmail.com>
2024-11-14 00:46:03 -0600

    LimpioFuncional
    Este jala

--- app/src/main/java/com/example/adaptadores/SettingsActivity.java
+++ app/src/main/java/com/example/adaptadores/SettingsActivity.java
@@ -7,6 +7,6 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.settingsactivity);
     }
 }
